9cccca9ad9f1549c4cd581e3c875dc157a7527e3
Michelle Wong <michellekwong2@gmail.com>
2019-11-19 17:01:31 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let tasks = document.querySelector("#tasks")
+  let submit = document.getElementById("button")
+
+  submit.addEventListener("click", function(e){
+
+  let form = submit.parentNode
+  let newTask= form.querySelector("#new-task-description").value
+    
+  let li= document.createElement("li")
+
+  li.innerText = newTask
+
+  function deleteTask(e){
+    let li = del.parentNode 
+    li.remove()
+  }
+
+  let del = document.createElement("button")
+    del.className= "delete"
+    del.innerText = "delete"
+
+  del.addEventListener("click", deleteTask)
+   
+  li.append(del)
+  tasks.append(li)
+  e.preventDefault()
+  })
+
+
+
 });
+
+
+
